a21b8f7e03ea72a833b0151a0439438c359cc628
vinitkumar128 <vivek.raj915568@gmail.com>
2025-10-31 00:15:02 +0530

    Delete assignment.sh

--- assignment.sh
+++
@@ -1,4 +0,0 @@
-#assignment
-# write a script to print addition,substraction of two number by 
-# a) taking input from user at the time of execution of script thint use read 
-# b) by taking input passed while calling script hint use $1 and $
